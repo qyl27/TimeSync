106fbcc2688a412864278da6887a573a01917758
秋雨落 <i@rain.cx>
2024-03-17 20:16:10 +0800

    :tada: Init.
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ src/main/java/cx/rain/mc/timesync/TimeSync.java
@@ -0,0 +1,17 @@
+package cx.rain.mc.timesync;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TimeSync extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
